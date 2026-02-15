745b30b0c64ee54af2a68c39fb9d8e92b13bc05f
qossiim <qosimtoirov2000@gmail.com>
2026-02-15 13:54:08 +0900

    fix: develop burak screen based components

--- src/index.tsx
+++ src/index.tsx
@@ -7,14 +7,18 @@ import reportWebVitals from "./reportWebVitals";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./app/MaterialTheme";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./css/index.css";
+
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}> 
         <CssBaseline />
-        <App />
+        <Router>
+          <App />
+          </Router>
       </ThemeProvider>
     </Provider>
   </React.StrictMode>,
