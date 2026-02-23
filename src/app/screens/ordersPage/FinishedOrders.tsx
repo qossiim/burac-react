33839b727e15ab03b18b402bb4cbe325cd1e70a1
qossiim <qosimtoirov2000@gmail.com>
2026-02-23 22:05:03 +0900

    feat: publish orderPage screen  component

---
+++ src/app/screens/ordersPage/FinishedOrders.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { Box, Stack } from "@mui/joy";
+import Button from "@mui/material/Button";
+import TabPanel from "@mui/lab/TabPanel";
+import moment from "moment";
+
+export default function FinishedOrders() {
+  return (
+    <TabPanel value={"3"}>
+      <Stack>
+        {[1, 2].map((ele, index) => {
+          return (
+            <Box key={index} className={"order-main-box"}>
+              <Box className={"order-box-scroll"}>
+                {[1, 2, 3].map((ele2, index2) => {
+                  return (
+                    <Box key={index2} className={"orders-name-price"}>
+                      <img
+                        src={"/img/lavash.webp"}
+                        alt=""
+                        className={"order-dish-img"}
+                      />
+                      <p className={"title-dish"}>Lavash</p>
+                      <Box className={"price-box"}>
+                        <p>$11</p>
+                        <img src={"/icons/close.svg"} />
+                        <p>2</p>
+                        <img src={"/icons/pause.svg"} alt="" />
+                        <p style={{ marginLeft: "15px" }}>$22</p>
+                      </Box>
+                    </Box>
+                  );
+                })}
+              </Box>
+
+              <Box className={"total-price-box"}>
+                <Box className={"box-total"}>
+                  <p>Product price</p>
+                  <p>$18</p>
+                  <img
+                    src={"/icons/plus.svg"}
+                    style={{ marginLeft: "20px" }}
+                    alt=""
+                  />
+                  <p>Deliver cost</p>
+                  <p>$2</p>
+                  <img 
+                    src={"/icons/pause.svg"}
+                    style={{ marginLeft: "20px" }}
+                  />
+                  <p>Total</p>
+                  <p>$20</p>
+                </Box>
+              </Box>
+            </Box>
+          );
+        })}
+
+        {false && (
+          <Box display={"flex"} flexDirection={"row"} justifyContent={"center"}>
+            <img
+              src={"/icons/noimage-list.svg"}
+              style={{ width: 300, height: 300 }}
+              alt=""
+            />
+          </Box>
+        )}
+      </Stack>
+    </TabPanel>
+  );
+}
--- src/app/screens/ordersPage/index.tsx
+++ src/app/screens/ordersPage/index.tsx
@@ -1,6 +1,119 @@
-import { Container } from "@mui/material";
-
+import { SyntheticEvent, useState } from "react";
+import { Box, Container, Stack } from "@mui/material";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import PausedOrders from "./PausedOrders";
+import FinishedOrders from "./FinishedOrders";
+import ProcessOrders from "./ProcessOrders";
+import "../../../css/order.css";
 
 export default function OrdersPage() {
-  return <Container>OrdersPage</Container>;
+  const [value, setValue] = useState("1");
+
+  /** HANDLERS **/
+  const handleChange = (e: SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={"order-page"}>
+      <Container className={"order-container"}>
+        <Stack className={"order-left"}>
+          <TabContext value={value}>
+            <Box className={"order-nav-frame"}>
+              <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                <Tabs
+                  value={value}
+                  onChange={handleChange}
+                  aria-label="basic tabs example"
+                  className={"table-list"}
+                >
+                  <Tab label="PAUSED ORDERS" value={"1"} />
+                  <Tab label="PROCESS ORDERS" value={"2"} />
+                  <Tab label="FINISHED ORDERS" value={"3"} />
+                </Tabs>
+              </Box>
+            </Box>
+            <Stack className={"order-main-content"}>
+              <PausedOrders />
+              <ProcessOrders />
+              <FinishedOrders />
+            </Stack>
+          </TabContext>
+        </Stack>
+
+        <Stack className={"order-right"}>
+          <Box className={"order-info-box"}>
+            <Box className={"member-box"}>
+              <div className={"order-user-img"}>
+                <img
+                  alt=""
+                  src={"/icons/default-user.svg"}
+                  className={"order-user-avatar"}
+                />
+                <div className={"order-user-icon-box"}>
+                  <img
+                    alt=""
+                    src={"/icons/user-badge.svg"}
+                    className={"order-user-prof-img"}
+                  />
+                </div>
+              </div>
+              <span className={"order-user-name"}>TOIROV QOSIM</span>
+              <span className={"order-user-prof"}> JOHN</span>
+            </Box>
+            <Box className={"liner"}></Box>
+            <Box className={"order-user-address"}>
+              <div style={{ display: "flex" }}>
+                <LocationOnIcon fontSize="medium" />
+              </div>
+              <div className={"spec-address-txt"}>UZBEKISTAN BUKHARA</div>
+            </Box>
+          </Box>
+          <Box className={"order-info-box"}>
+            <input
+              type={"text"}
+              name={"cardNumber"}
+              placeholder={"Card number : **** 8600 2000 0710"}
+              className={"card-input"}
+            />
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <input
+                type={"text"}
+                name={"cardPeriod"}
+                placeholder={"07/ 10"}
+                className={"card-half-input"}
+              />
+              <input
+                type={"text"}
+                name={"cardCVV"}
+                placeholder={"CVV : 080"}
+                className={"card-half-input"}
+              />
+            </div>
+            <input
+              type={"text"}
+              name={"cardCreator"}
+              placeholder={"Toirov Qosim"}
+              className={"card-input"}
+            />
+            <div className={"cards-box"}>
+              <img alt="" src={"/icons/western-card.svg"} />
+              <img alt="" src={"/icons/master-card.svg"} />
+              <img alt="" src={"/icons/paypal-card.svg"} />
+              <img alt="" src={"/icons/visa-card.svg"} />
+            </div>
+          </Box>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
