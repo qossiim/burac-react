e2976577fc95275e8d1e4d251015b26190c5bc1e
qossiim <qosimtoirov2000@gmail.com>
2026-03-11 13:28:34 +0900

    feat: develop AppRootState and HomePage type integrations

---
+++ src/lib/enums/member.enum.ts
@@ -0,0 +1,10 @@
+export enum MemberType {
+  USER = "USER",
+  RESTAURANT = "RESTAURANT",
+}
+
+export enum MemberStatus {
+  ACTIVE = "ACTIVE",
+  BLOCk = "BLOCk",
+  DELETE = "DELETE",
+}
---
+++ src/lib/types/screen.ts
@@ -0,0 +1,18 @@
+import { Member } from "./member";
+import { Product } from "./product";
+
+/** React App State */
+export interface AppRootState {
+    homePage: HomePageState;
+}
+
+/** HomePage*/
+export interface HomePageState {
+    popularDishes: Product[];
+    newDishes: Product[];
+    topUsers: Member[];
+}
+
+/** ProductaPage */
+
+/** OrdersPage */
